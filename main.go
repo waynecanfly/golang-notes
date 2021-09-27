5eee356c0ad730824b938396ee10c6ef59f1cbd6
waynecanfly <1370153124@qq.com>
2021-09-27 09:58:51 +0800

    update

--- main.go
+++ main.go
@@ -2,15 +2,6 @@ package main
 
 import "practice/generate_json"
 
-type Server struct {
-    ServerName string
-    ServerIP string
-}
-
-type Serverslice struct {
-    Servers []Server
-}
-
 func main() {
     generate_json.GenJson()
 }
